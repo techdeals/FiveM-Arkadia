9f8d2e4c4a7faa97de29a44cac516902a1a357d3
ItsikNox <46906739+ItsikNox@users.noreply.github.com>
2019-09-16 03:57:20 +0200

    Remove & Modif

--- resources/kick_restart/server.lua
+++
@@ -1,4 +0,0 @@
-RegisterServerEvent("kickForRestart")
-AddEventHandler("kickForRestart", function()
-	DropPlayer(source, "Toutes les situations du jeu de rôle se terminent automatiquement. Vos progrès ont été sauvegardés. Raison : Redémarrage du serveur") --Reason of why the player got kicked
-end)
